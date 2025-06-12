9cdccb943e6091c8463ac56a9f9c8aa12052694b
Tony <48512979+fedor22i@users.noreply.github.com>
2025-06-12 16:44:35 +0000

    add jenkins file

---
+++ jenkins.groovy
@@ -0,0 +1,62 @@
+pipeline {
+agent any
+    parameters {
+        choice(
+            name: 'OS',
+            choices: ['linux', 'darwin', 'windows'],
+            description: 'Target operating system'
+        )
+        choice(
+            name: 'ARCH',
+            choices: ['amd64', 'arm64'],
+            description: 'Target architecture'
+        )
+    }
+    
+    environment {
+        REPO = 'https://github.com/fedor22i/kbot'
+        BRANCH = 'main'
+    }
+    stages {
+        
+        stage("clone") {
+            steps {
+            echo 'CLONE REPOSITORY'
+                git branch: "${BRANCH}", url: "${REPO}"
+            }
+        }
+        
+        stage("test") {
+            steps {
+            echo 'TEST EXECUTION STARTED'
+                sh 'make test'
+            }
+        }
+        
+        stage("build") {
+            steps {
+            echo 'BUILD EXECUTION STARTED'
+                sh 'make build'
+            }
+        }
+        
+        stage("image") {
+            steps {
+                script {
+                    echo 'BUILD EXECUTION STARTED'
+                    sh 'make image'
+                }
+            }
+        }
+        
+        stage("push") {
+            steps {
+                script {
+                    docker.withRegistry( '', 'dockerhub') {
+                        sh 'make push'
+                    }
+                }
+            }
+        }
+    }
+}
